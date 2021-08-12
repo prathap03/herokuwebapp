e46e3643bc16ad74a3f10b7c80dd34009d706e5b
Joe Prathap PJ <jonitest92@gmail.com>
2021-08-12 21:07:50 +0530

    heroku website hosting

---
+++ backend/data.js
@@ -0,0 +1,40 @@
+export default {
+    products: [
+        {
+            id: 1,
+            name: 'Cement',
+            image: '/images/d1.jpeg',
+            price: 500,
+            rating: 5,
+        },
+        {
+            id: 2,
+            name: 'Structural Steel',
+            image: '/images/d2.png',
+            price: 500,
+            rating: 5,
+        },
+        {
+            id: 3,
+            name: 'rebars',
+            image: '/images/d3.jpg',
+            price: 500,
+            rating: 5,
+        },
+        {
+            id: 4,
+            name: 'Bitumen',
+            image: '/images/d4.jpg',
+            price: 500,
+            rating: 5,
+        },
+        {
+            id: 5,
+            name: 'Sand(River)',
+            image: '/images/d5.jpeg',
+            price: 500,
+            rating: 5,
+        },
+
+    ],
+}
